17f2a1f5c86291a33b377d300dacdb73f3e6581d
Md Sakib <sakibbirol44@gmail.com>
2023-11-14 14:03:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    $('.client_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:1000,
+        dots:true,
+    });
+  })
